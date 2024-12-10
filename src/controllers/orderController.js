37ebac19ba3b08eb0705037df76d964e2aba0896
AndreiTroitskii <troitskiya@yandex.ru>
2024-12-11 01:46:31 +0530

    feat: add updating order

--- src/controllers/orderController.js
+++ src/controllers/orderController.js
@@ -8,5 +8,27 @@ const createOrder = async (req, res) => {
         res.status(500).send(error);
     }
 }
+const getOrder = async (req, res) => {
+    const id = req.params.id;
+    const now = new Date();
+    try {
+        const currentDate = `${now.getFullYear()}-${now.getMonth() + 1}-${now.getDate()}`;
+        const response = await apiService.get(`transactions.getTransactions?date_from=${currentDate}&date_to=${currentDate}&per_page=1000`);
+        const orders = response.data.find(({transaction_id}) => transaction_id === Number(id));
+        res.status(200).send(orders);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+}
+const updateOrder = async (req, res) => {
+    try {
+        const response = await apiService.post(`transactions.addTransactionProduct`, req.body);
+        res.status(200).send(response);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+}
 
 module.exports.createOrder = createOrder;
+module.exports.getOrder = getOrder;
+module.exports.updateOrder = updateOrder;
